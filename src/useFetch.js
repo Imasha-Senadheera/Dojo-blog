de86b2d1d34bf71f054d488f0156812f4489921f
Imasha Senadheera <imashasenadheera326@gmail.com>
2023-07-15 21:34:54 +0530

    Changers 8

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,3 +1,4 @@
+import { cleanup } from '@testing-library/react';
 import { useState, useEffect } from 'react';
 
 const useFetch = (url) => {
@@ -6,28 +7,36 @@ const useFetch = (url) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    const abortCont = new AbortController;
+
     setTimeout(() => {
-      fetch(url)
-      .then(res => {
-        if (!res.ok) { // error coming back from server
-          throw Error('could not fetch the data for that resource');
-        } 
-        return res.json();
-      })
-      .then(data => {
-        setIsPending(false);
-        setData(data);
-        setError(null);
-      })
-      .catch(err => {
-        // auto catches network / connection error
-        setIsPending(false);
-        setError(err.message);
-      })
+      fetch(url, {singnal : abortCont.signal})
+        .then(res => {
+          if (!res.ok) { // error coming back from server
+            throw Error('could not fetch the data for that resource');
+          }
+          return res.json();
+        })
+        .then(data => {
+          setIsPending(false);
+          setData(data);
+          setError(null);
+        })
+        .catch(err => {
+          if (err.name === 'AbortError'){
+            console.log('fetch aborted')
+          } else {
+          // auto catches network / connection error
+          setIsPending(false);
+          setError(err.message);
+          }
+        })
     }, 1000);
+
+    return () => abortCont.abort();
   }, [url])
 
   return { data, isPending, error };
 }
- 
+
 export default useFetch;
